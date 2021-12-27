833b0978a3da56864b8c0e2d48286e78612cf608
64830 <648303143@qq.com>
2021-12-27 11:47:52 +0800

    常量类

---
+++ src/constant/Constant.java
@@ -0,0 +1,18 @@
+package constant;
+
+/**
+ * @author zzzZqy
+ * @Description
+ * @create 2021-11-07 18:55
+ */
+public class Constant {
+    public static final String PATH = "D:\\download\\";
+
+    public static final int BYTE_SIZE = 1024 * 100;
+
+    public static final int MB = 1024 * 1024;
+
+    public static final int THREAD_NUM = 6;
+
+}
+
